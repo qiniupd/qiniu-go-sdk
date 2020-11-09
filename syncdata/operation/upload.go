d1104b7735267a365dab913895177c21265950fa
longbai <slongbai@gmail.com>
2020-11-09 18:44:10 +0800

    add put data

--- syncdata/operation/upload.go
+++ syncdata/operation/upload.go
@@ -3,6 +3,8 @@ package operation
 import (
 	"context"
 	"encoding/json"
+	"github.com/qiniupd/qiniu-go-sdk/x/bytes.v7"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -30,7 +32,7 @@ func (p *Uploader) makeUptoken(policy *kodo.PutPolicy) string {
 	return qbox.SignWithData(p.credentials, b)
 }
 
-func (p *Uploader) Upload(file string, key string) error {
+func (p *Uploader) UploadData(data []byte, key string) (err error) {
 	t := time.Now()
 	defer func() {
 		log.Println("up time ", key, time.Now().Sub(t))
@@ -41,6 +43,62 @@ func (p *Uploader) Upload(file string, key string) error {
 		Expires: 3600*24 + uint32(time.Now().Unix()),
 	}
 
+	upToken := p.makeUptoken(&policy)
+
+	var uploader = q.NewUploader(1, &q.UploadConfig{
+		UpHosts:        p.upHosts,
+		UploadPartSize: p.partSize,
+		Concurrency:    p.upConcurrency,
+	})
+	for i := 0; i < 3; i++ {
+		err = uploader.Put2(context.Background(), nil, upToken, key, bytes.NewReader(data), int64(len(data)), nil)
+		if err == nil {
+			break
+		}
+		log.Println("small upload retry", i, err)
+	}
+	return
+}
+
+func (p *Uploader) UploadDataReader(data io.Reader, size int, key string) (err error) {
+	t := time.Now()
+	defer func() {
+		log.Println("up time ", key, time.Now().Sub(t))
+	}()
+	key = strings.TrimPrefix(key, "/")
+	policy := kodo.PutPolicy{
+		Scope:   p.bucket + ":" + key,
+		Expires: 3600*24 + uint32(time.Now().Unix()),
+	}
+
+	upToken := p.makeUptoken(&policy)
+
+	var uploader = q.NewUploader(1, &q.UploadConfig{
+		UpHosts:        p.upHosts,
+		UploadPartSize: p.partSize,
+		Concurrency:    p.upConcurrency,
+	})
+
+	for i := 0; i < 3; i++ {
+		err = uploader.Put2(context.Background(), nil, upToken, key, data, int64(size), nil)
+		if err == nil {
+			break
+		}
+		log.Println("small upload retry", i, err)
+	}
+	return
+}
+
+func (p *Uploader) Upload(file string, key string) (err error) {
+	t := time.Now()
+	defer func() {
+		log.Println("up time ", key, time.Now().Sub(t))
+	}()
+	key = strings.TrimPrefix(key, "/")
+	policy := kodo.PutPolicy{
+		Scope:   p.bucket + ":" + key,
+		Expires: 3600*24 + uint32(time.Now().Unix()),
+	}
 	upToken := p.makeUptoken(&policy)
 
 	f, err := os.Open(file)
@@ -60,32 +118,28 @@ func (p *Uploader) Upload(file string, key string) error {
 		Concurrency:    p.upConcurrency,
 	})
 
-	if fInfo.Size() <= 16*1024*1024 {
-		err = uploader.PutFile(context.Background(), nil, upToken, key, file, nil)
-		if err == nil {
-			return nil
+	if fInfo.Size() <= p.partSize*1024*1024 {
+		for i := 0; i < 3; i++ {
+			err = uploader.Put2(context.Background(), nil, upToken, key, f, fInfo.Size(), nil)
+			if err == nil {
+				break
+			}
+			log.Println("small upload retry", i, err)
 		}
-		log.Println("small upload retry0", err)
-		err = uploader.PutFile(context.Background(), nil, upToken, key, file, nil)
-		log.Println("small upload retry1", err)
-		return err
+		return
 	}
 
-	err = uploader.Upload(context.Background(), nil, upToken, key, f, fInfo.Size(), nil,
-		func(partIdx int, etag string) {
-			log.Println("callback", partIdx, etag)
-		})
-	if err == nil {
-		return nil
+	for i := 0; i < 3; i++ {
+		err = uploader.Upload(context.Background(), nil, upToken, key, f, fInfo.Size(), nil,
+			func(partIdx int, etag string) {
+				log.Println("callback", partIdx, etag)
+			})
+		if err == nil {
+			break
+		}
+		log.Println("part upload retry", i, err)
 	}
-	log.Println("part upload retry0", err)
-	err = uploader.Upload(context.Background(), nil, upToken, key, f, fInfo.Size(), nil,
-		func(partIdx int, etag string) {
-			log.Println("callback", partIdx, etag)
-		})
-
-	log.Println("part upload retry1", err)
-	return err
+	return
 }
 
 func NewUploader(c *Config) *Uploader {
