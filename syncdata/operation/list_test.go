d5c9ebe7ff8a32bca347f8362ec942576d68ed39
chupei <chupei@qiniu.com>
2021-03-19 15:15:30 +0800

    add list ut test

--- syncdata/operation/list_test.go
+++ syncdata/operation/list_test.go
@@ -2,9 +2,11 @@ package operation
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"math/rand"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var (
@@ -17,6 +19,7 @@ func init() {
 	cfg = newTestConfig()
 	uploader = NewUploader(cfg)
 	lister = NewLister(cfg)
+	rand.Seed(time.Now().UnixNano())
 }
 
 func TestListRename(t *testing.T) {
@@ -68,15 +71,48 @@ func TestListStat(t *testing.T) {
 	//upload file
 	testKey := fmt.Sprintf("test_list_%d", rand.Int())
 	testNonKey := fmt.Sprintf("test_list_%d", rand.Int())
-	err := uploader.Upload("config.go", testKey)
+	err := uploader.Upload("../../data/upload.jpg", testKey)
 	assert.NoError(t, err)
 	defer lister.Delete(testKey)
 
 	//check liststat success
 	result := lister.ListStat([]string{testKey})
 	assert.Equal(t, testKey, result[0].Name)
+	assert.Equal(t, int64(51523), result[0].Size)
 
 	//check liststst non-exist key
 	result = lister.ListStat([]string{testNonKey})
 	assert.Equal(t, int64(-1), result[0].Size)
+
+	result = lister.ListStat([]string{testKey, testNonKey})
+	assert.Equal(t, 2, len(result))
+}
+
+func TestStat(t *testing.T) {
+	//upload file
+	testKey := fmt.Sprintf("test_list_%d", rand.Int())
+	err := uploader.Upload("../../data/upload.jpg", testKey)
+	assert.NoError(t, err)
+
+	//stat and check result
+	result, err := lister.Stat(testKey)
+	assert.NoError(t, err)
+	assert.Equal(t, "FkBhdo9odL2Xjvu-YdwtDIw79fIL", result.Hash)
+	assert.Equal(t, int64(51523), result.Fsize)
+	assert.Equal(t, "image/jpeg", result.MimeType)
+}
+
+func TestDelete(t *testing.T) {
+	//upload file
+	testKey := fmt.Sprintf("test_list_%d", rand.Int())
+	err := uploader.Upload("config.go", testKey)
+	assert.NoError(t, err)
+
+	//delete file
+	err = lister.Delete(testKey)
+	assert.NoError(t, err)
+
+	//stat file and check file has been delete
+	_, err = lister.Stat(testKey)
+	assert.Error(t, err)
 }
